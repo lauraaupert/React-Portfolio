88975d45b7352ce8911282182523300fb9a44ff2
Laura Aupert <aupert.laura@gmail.com>
2021-04-04 14:16:18 -0400

    Src folder: Swapped for portfolio source folder

---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import NavTabs from "./NavTabs";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Blog from "./pages/Blog";
+import Contact from "./pages/Contact";
+
+class PortfolioContainer extends Component {
+  state = {
+    currentPage: "Home"
+  };
+
+  handlePageChange = page => {
+    this.setState({ currentPage: page });
+  };
+
+  // switches pages
+  renderPage = () => {
+    switch (this.state.currentPage) {
+      case "About":
+        return <About />
+
+        case "Blog":
+          return <Blog />
+
+          case "Contact":
+            return <Contact />
+
+      default: 
+      return<Home />
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <NavTabs
+          currentPage={this.state.currentPage}
+          handlePageChange={this.handlePageChange}
+        />
+        {this.renderPage()}
+      </div>
+    );
+  }
+}
+
+export default PortfolioContainer;
